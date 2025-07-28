5083b65fc04f10ae16008da6603468d28243da98
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-28 23:17:25 +0530

    assignment4

---
+++ Collections/List/assignment4.java
@@ -0,0 +1,20 @@
+package Collections.List;
+
+import java.util.ArrayList;
+
+public class assignment4
+{
+    public static void main(String[] args) {
+        ArrayList<Number> numbers = new ArrayList<>();
+
+        numbers.add(10);        
+        numbers.add(15.5f);      
+        numbers.add(20.75);     
+        numbers.add(100L);      
+
+        System.out.println("Stored Numbers:");
+        for (Number num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
