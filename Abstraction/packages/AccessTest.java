4cb684c31d3488284c17063455e05060aa4d6b51
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-28 12:29:42 +0530

    packages

---
+++ Abstraction/packages/AccessTest.java
@@ -0,0 +1,11 @@
+package Abstraction.packages;
+import Abstraction.packages.test.foundation;
+
+public class AccessTest {
+    public static void main(String[] args) {
+        foundation f = new foundation();
+
+        System.out.println(f.var4);
+    }
+    
+}
---
+++ Abstraction/packages/test/foundation.java
@@ -0,0 +1,11 @@
+package Abstraction.packages.test;
+
+public class foundation {
+    private int var1=1;
+    int var2=2;
+    protected int var3=3;
+    public int var4=4;
+    
+}
+
+
