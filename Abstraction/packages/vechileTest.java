9be7246038d8da8536c775bf8cb669cc9bdb4c81
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-28 19:51:43 +0530

    fourwheeler

--- Abstraction/packages/vechileTest.java
+++ Abstraction/packages/vechileTest.java
@@ -2,12 +2,17 @@ package Abstraction.packages;
 
 import Abstraction.packages.automobiles.twowheeler.Hero;
 import Abstraction.packages.automobiles.twowheeler.Honda;
+import Abstraction.packages.automobiles.fourwheelers.Ford;
+import Abstraction.packages.automobiles.fourwheelers.Logan;
 
 public class vechileTest {
 
     public static void main(String[] args) {
         Hero h = new Hero();
         Honda hh = new Honda();
+        Ford ford = new Ford();
+        Logan logan = new Logan();
+
         System.out.println(h.getModelName());
         System.out.println(h.getRegistrationNumber());
         System.out.println(h.getOwnerName());
@@ -18,6 +23,22 @@ public class vechileTest {
         System.out.println(hh.getOwnerName());
         System.out.println(hh.getSpeed());
         hh.cdplayer();
+
+        System.out.println("Logan Details:");
+        System.out.println("Model: " + logan.getModelName());
+        System.out.println("Reg No: " + logan.getRegistrationNumber());
+        System.out.println("Owner: " + logan.getOwnerName());
+        System.out.println("Speed: " + logan.getSpeed());
+        logan.gps();
+
+        System.out.println();
+
+        System.out.println("Ford Details:");
+        System.out.println("Model: " + ford.getModelName());
+        System.out.println("Reg No: " + ford.getRegistrationNumber());
+        System.out.println("Owner: " + ford.getOwnerName());
+        System.out.println("Speed: " + ford.getSpeed());
+        ford.tempControl();
 
     }
 
