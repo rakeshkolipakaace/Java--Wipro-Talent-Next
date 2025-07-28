0f9f56db42ac12c05c3aedc5c139d587107ddca7
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-28 14:31:26 +0530

    package assignment 2

---
+++ Abstraction/packages/shipTest.java
@@ -0,0 +1,14 @@
+package Abstraction.packages;
+
+import Abstraction.packages.automobiles.compartment;
+
+
+public class shipTest {
+    public static void main(String[] args) {
+
+        compartment c = new compartment(10, 20, 30);
+        c.display();
+        
+    }
+    
+}
