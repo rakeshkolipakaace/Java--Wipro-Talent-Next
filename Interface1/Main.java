034f0ddb706b2122ef1d1df8553a333ebfb0328a
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-28 20:35:48 +0530

    interface1 main.java

---
+++ Interface1/Main.java
@@ -0,0 +1,12 @@
+package Interface1;
+
+import Interface1.vehicle.car;
+public class Main {
+    public static void main(String[] args) {
+
+        car c=new car();
+        c.message();
+        
+    }
+    
+}
