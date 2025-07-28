068d5ed71b3a9de2ae99e73ff2ca94591bddde69
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-28 23:34:36 +0530

    Collections/List/assignment7.java

---
+++ Collections/List/assignment7.java
@@ -0,0 +1,15 @@
+package Collections.List;
+
+public class assignment7 {
+    private int empId;
+    private String empName;
+
+    public assignment7(int empId, String empName) {
+        this.empId = empId;
+        this.empName = empName;
+    }
+
+    public String toString() {
+        return "Employee ID: " + empId + ", Name: " + empName;
+    }
+}
