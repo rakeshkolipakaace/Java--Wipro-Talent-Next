fb2dc4fb12b5734447b82005cabe24a53f01bb67
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-29 10:48:51 +0530

    assignment1

---
+++ Collections/Sets/assignment1.java
@@ -0,0 +1,29 @@
+package Collections.Sets;
+
+import java.util.HashSet;
+
+public class assignment1 {
+    HashSet<String> H1 = new HashSet<>();
+
+    public HashSet<String> saveCountryNames(String countryName) {
+        H1.add(countryName);
+        return H1;
+    }
+
+    public String getCountry(String countryName) {
+        if (H1.contains(countryName)) {
+            return countryName;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        assignment1 obj1 = new assignment1();
+        obj1.saveCountryNames("India");
+        obj1.saveCountryNames("Australia");
+
+        System.out.println(obj1.getCountry("India"));
+        System.out.println(obj1.getCountry("Russia"));
+
+    }
+}
