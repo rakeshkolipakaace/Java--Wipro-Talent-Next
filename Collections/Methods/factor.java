4220762f95b4e9d5e37f544986c2e0a3fd7e3ca0
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-07-29 15:55:19 +0530

    factor.java

---
+++ Collections/Methods/factor.java
@@ -0,0 +1,13 @@
+package Collections.Methods;
+
+public class factor {
+    public int factorial(int n) {
+        if (n < 0)
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
